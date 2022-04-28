86b03aa2d92f71ec1dc3c1fe036d03356ef295be
dannaemartinez <dannae.martinez@enroutesystems.com>
2022-04-28 15:40:35 -0500

    Create with Push

--- cucumber/features/step-definitions/steps.js
+++ cucumber/features/step-definitions/steps.js
@@ -4,6 +4,39 @@ const HomePage = require('../page-objects/home.page');
 const NavBar = require('../page-objects/global/navbar');
 const MovieList = require('../page-objects/movie.list.page');
 
+// const pages = {
+//     home: HomePage
+// }
+
+// Given(/^I am on the (\w+) page$/, 
+//     async (page) => await pages[page].open());
+
+// //When(/^on the navbar I select category "(Todo|Títulos|Episodios de TV)"$/, 
+//    // async (category) => await NavBar.searchBar.selectCategory(category));
+
+//    When(/^on the navbar I search "(\s+)"$/, 
+//     async (category) => await NavBar.searchBar.selectCategory(category));
+
+// Then(/^I should see the category dropdown now matches "(Todo|Títulos|Episodios de TV)"$/, 
+//     async (category) => {
+//         // This is a destructuring asignment
+//         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+//         const { categoryDropdown} = NavBar.searchBar;
+//         await categoryDropdown.waitForDisplayed({
+//             timeout: 1000,
+//             timeoutMsg: 'The Category dropdown was not displayed'
+//         });
+//         const text = await categoryDropdown.getText();
+//         // Assertions docs: https://jestjs.io/docs/using-matchers
+//         /**
+//         * Why does this line doesn't use await?
+//         * Answer: when selenium is consumed we send a HTTP request to the selenium API
+//         *         this force the callback to go to the callback stack, forcing us to use async/await
+//         * https://medium.com/@Rahulx1/understanding-event-loop-call-stack-event-job-queue-in-javascript-63dcd2c71ecd
+//         */
+//         expect(text).toMatch(category);
+//     });
+
 const pages = {
     home: HomePage
 }
@@ -11,18 +44,17 @@ const pages = {
 Given(/^I am on the (\w+) page$/, 
     async (page) => await pages[page].open());
 
-When(/^on the navbar I select category "(All|Titles|TV Episodes)"$/, 
-    async (category) => await NavBar.searchBar.selectCategory(category));
+   When(/^on the navbar I search "(The Batman)"$/, (movie)=>{
+       NavBar.searchBar.input.setText(movie)
+   }) 
 
-When(/^on the navbar I search "(The Batman)"$/, (movie)=> {
-    MovieList.rowHyperlink(movie);
-});
+   //Crear then basado en los 
 
-Then(/^I should see the category dropdown now matches "(All|Titles|TV Episodes)"$/, 
+Then(/^I should see the category dropdown now matches "(Todo|Títulos|Episodios de TV)"$/, 
     async (category) => {
         // This is a destructuring asignment
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-        const { categoryDropdown } = NavBar.searchBar;
+        const { categoryDropdown} = NavBar.searchBar;
         await categoryDropdown.waitForDisplayed({
             timeout: 1000,
             timeoutMsg: 'The Category dropdown was not displayed'
@@ -37,3 +69,4 @@ Then(/^I should see the category dropdown now matches "(All|Titles|TV Episodes)"
         */
         expect(text).toMatch(category);
     });
+
