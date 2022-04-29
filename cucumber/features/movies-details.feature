3c516f7d662036b0b32347052a79cc4767e1bfe0
= <pakoo_15@hotmail.com>
2022-04-28 21:48:30 -0500

    Create first scenario

--- cucumber/features/movies-details.feature
+++ cucumber/features/movies-details.feature
@@ -2,8 +2,8 @@ Feature: "The Batman" Movie Details Page
 
   Scenario Outline: As a user, I should be able the see if director is Matt Reeves & and than Robert Pattison is 1 of the "Stars"
     Given I am on the home page
-    And on the navbar I search "The Batman"
-    When on the page I select the first movie
+    When on the navbar I search "The Batman"
+    Then on the page I select "The Batman" movie
     Then I should see the director "Matt Reeves" 
-    And  I should see the star "Robert Pattison"
+    Then I should see the star "Robert Pattinson"
         
--- cucumber/features/page-objects/global/movie.list.page.js
+++ cucumber/features/page-objects/global/movie.list.page.js
@@ -1,10 +1,18 @@
 class MovieList {
-  constructor(parentLocator){
-      this.parentLocator = `.find.section:nth-of-child(${tableId})`; //traido de css
-  }
+  
 
-  movieName(text){
-      return $(this.parentLocator).$(`a=${text}`)
+  async selectMovie(name){
+    const movieList = $(".findSection")
+    await movieList.waitForDisplayed({ 
+      timeout: 2500, 
+      timeoutMsg: 'The list of movies are not displayed' 
+    });
+    const link = $(".findSection").$(`a*=${name}`);
+    await link.waitForClickable({ 
+      timeout: 2500, 
+      timeoutMsg: 'The list of movies are not displayed' 
+    });
+    link.click()
   }
 }
 
