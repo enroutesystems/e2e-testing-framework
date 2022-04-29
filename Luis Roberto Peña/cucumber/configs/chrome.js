1629e0f9526c7d728f45707f00b587dd7dd3bfe0
LuisRococo <luisrobertopenav@gmail.com>
2022-04-29 15:36:26 -0700

    Refactoring details

--- Luis Roberto Peña/cucumber/configs/chrome.js
+++ Luis Roberto Peña/cucumber/configs/chrome.js
@@ -1,11 +1,8 @@
 const config = require("../wdio.conf");
 config.capabilities = [
   {
-    // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-    // grid with only 5 firefox instances available you can make sure that not more than
-    // 5 instances get started at a time.
     maxInstances: 5,
-    //
+
     browserName: "chrome",
     acceptInsecureCerts: true,
     "goog:chromeOptions": {
@@ -13,11 +10,8 @@ config.capabilities = [
         "intl.accept_languages": "en,en-US"
       }
     }
-    // If outputDir is provided WebdriverIO can capture driver session logs
-    // it is possible to configure which logTypes to include/exclude.
-    // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-    // excludeDriverLogs: ['bugreport', 'server'],
   }
 ];
+config.services = ["chromedriver"];
 
 exports.config = config;
