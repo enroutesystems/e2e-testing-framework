91c145f59304e84be13bc8a6e964eda6a7c87d63
Anisbeflo <abejarano0@ucol.mx>
2022-04-29 16:55:45 -0500

    Fixed chrome browser in Spanish instead of English

--- AngelBejarano/cucumber/features/page-objects/movie.list.page.js
+++ AngelBejarano/cucumber/features/page-objects/movie.list.page.js
@@ -1,5 +1,5 @@
-class MovieList { 
-    
+class MovieList {
+
     constructor(tableId = 1) {
         this.parentLocator = `.find-section:nth-of-child(${tableId})`;
     }
@@ -12,11 +12,11 @@ class MovieList {
         return $(`.result_text`).$(`a`);
         //return $(this.parentLocator).$(`.result_text=${text}`);
     }
-    
+
     rateValue(value) {
         return $(`.sc-7ab21ed2-1.jGRxWM`);
     }
-    
+
 }
 
 module.exports = new MovieList;
