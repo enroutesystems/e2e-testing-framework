090de49c2b8f9aa84dc8f2a806d184c2460dbb49
Renata Hurtado <55102925+renatahdo@users.noreply.github.com>
2022-04-29 20:00:47 -0500

    Add Renata's Dir

---
+++ renata/cucumber/features/page-objects/movie.list.page.js
@@ -0,0 +1,20 @@
+class MovieList { 
+    constructor() {
+        this.parentLocator = `.findSection .findList tr`;
+    }
+
+    get rowHyperlink() {
+        return $(this.parentLocator).$(`.result_text a`);
+    }
+
+    async goToMovie() {
+        const anchor = this.rowHyperlink;
+        await anchor.waitForClickable({
+            timeoutMsg: 1000,
+            timeoutMsg: `The table was not clickable`
+        });
+        await anchor.click();
+    }
+}
+
+module.exports = new MovieList;
